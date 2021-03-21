87ed96694270929945d7974574b48bd92223d7ca
nimzil <nima.amirzadeh@gmail.com>
2021-03-21 13:05:01 -0700

    Add StreamListener to query live tweets

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-import sys
-from tweepy import OAuthHandler, API
-import os
+import string, sys, time
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweet_client import twitter_auth
 
 
-def twitter_auth():
+class CustomListener(StreamListener):
     """
-    Setup Twitter Authentication.
-    :return: tweepy.oAuthHandler object
+    Custom StreamListener for streaming twitter data
     """
-    try:
-        API_KEY = os.environ.get('API_KEY')
-        API_SECRET = os.environ.get('API_SECRET')
-        BEARER_TOKEN = os.environ.get('TOKEN')
-        ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-        TOKEN_SECRET = os.environ.get('TOKEN_SECRET')
-    except KeyError:
-        sys.stderr.write('Twitter Env variables not set.')
-        sys.exit(1)
-    auth = OAuthHandler(API_KEY, API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
-    return auth
 
-def twitter_client():
+    def __init__(self, fname):
+        safe_fname = format_filename(fname)
+        self.outfile = f'stream_{safe_fname}.jsonl'
+
+    def on_data(self, data):
+        try:
+            with open(self.outfile, 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            sys.stderr.write(f"Error on_data: {e} \n")
+            time.sleep(5)
+        return True
+
+    def on_error(self, status):
+        if status == 420:
+            sys.stderr.write("Rate Limit Exceeded \n")
+            return False
+        else:
+            sys.stderr.write(f"Error {status} \n")
+            return True
+
+
+def format_filename(fname):
     """
-    Setup Twitter API Client
-    :return: tweepy.API object
+    Convert fname into a safe string for a filename.
+    :param fname:
+    :return: string
     """
+    return ''.join(convert_valid(one_char) for one_char in fname)
+
+
+def convert_valid(one_char):
+    """
+    Convert a character into a "_" if "invalid"
+    :param one_char:
+    :return: string
+    """
+    valid_chars = f"-_.{string.ascii_letters, string.digits}"
+    if one_char in valid_chars:
+        return one_char
+    else:
+        return "_"
+
+
+if __name__ == '__main__':
+    query = sys.argv[1:]  # list of CLI arguments
+    query_fname = " ".join(query)
     auth = twitter_auth()
-    api = API(auth)
-
-    return api
-
-
-
-
+    twitter_stream = Stream(auth, CustomListener(query_fname))
+    twitter_stream.filter(track=query, is_async=True)
---
+++ tweet_client.py
@@ -0,0 +1,32 @@
+import os, sys
+from tweepy import API, OAuthHandler
+
+
+def twitter_auth():
+    """
+    Setup Twitter Authentication.
+    :return: tweepy.oAuthHandler object
+    """
+    try:
+        API_KEY = os.environ.get('API_KEY')
+        API_SECRET = os.environ.get('API_SECRET')
+        BEARER_TOKEN = os.environ.get('TOKEN')
+        ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+        TOKEN_SECRET = os.environ.get('TOKEN_SECRET')
+    except KeyError:
+        sys.stderr.write('Twitter Env variables not set.')
+        sys.exit(1)
+    auth = OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
+    return auth
+
+
+def twitter_client():
+    """
+    Setup Twitter API Client
+    :return: tweepy.API object
+    """
+    auth = twitter_auth()
+    api = API(auth)
+
+    return api
